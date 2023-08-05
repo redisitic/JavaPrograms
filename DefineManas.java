47271e78fdc0f6785a3f144c8d7f0da097068705
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-08-05 18:03:18 +0530

    Commit DefineManas

--- DefineManas.java
+++ DefineManas.java
@@ -1,28 +1,34 @@
 import java.util.*;
+import java.io.File;
+
 class DefineManas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner bad = null;
+        try {
+            File file = new File("badwords.txt");
+            bad = new Scanner(file);
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+
+        String lines = bad.nextLine();
+        int no = Integer.parseInt(lines);
+        System.out.println(no);
+        String arr[] = new String[no];
+        for (int i = 0; i < no; i++) {
+            arr[i] = bad.nextLine();
+        }
+
         System.out.print("Enter the Person's name: ");
         String name = sc.nextLine();
         sc.close();
-        if(name == "" || name == " " || name == null){
+        if (name == "" || name == " " || name == null) {
             name = "Manas";
         }
-        System.out.println(" "); 
-        System.out.print(name + " is ");
-        String[] strAr = {"kutta", "bhenchod", "mawali", "bhosdike", "madarchod", "baap ki chut", "saala", "bhen ka loda", "chutiya"};
-        int arlen = strAr.length;
-        for(int i = 0; i < arlen; i++){
-            int len1 = arlen - 1;
-            int len2 = arlen - 2;
-            System.out.print(strAr[i]);
-            if(i == len2){
-                System.out.print(" and ");
-            }else if(i == len1){
-                System.out.print(".");
-            }else {
-                System.out.print(", ");
-            }
-        }
+        System.out.println(" ");
+        System.out.print(name + " is a ");
+        System.out.print(arr[(int) (Math.random() * no)] + ".\n");
+
     }
 }
