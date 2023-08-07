b6fd295aec34117b59fcf70b21ea4162d6f4fa19
Rhythm Juneja <66195447+redisitic@users.noreply.github.com>
2023-08-07 10:22:51 +0530

    Changed Background and formatted text.

--- CountTill10.java
+++ CountTill10.java
@@ -15,6 +15,7 @@ public class CountTill10 extends JFrame implements ActionListener{
         button = new JButton("Count");
         button.addActionListener(this);
         button.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+        button.setBackground(new Color(255, 255, 64));
 
         label = new JLabel("Total = ");
         label.setFont(new Font("Segoe UI", Font.BOLD, 20));
@@ -28,9 +29,11 @@ public class CountTill10 extends JFrame implements ActionListener{
         JLabel empty = new JLabel("");
 
         outlabel = new JLabel("");
-        outlabel.setFont(new Font("Segoe UI", Font.PLAIN, 20));
-        outlabel.setPreferredSize(new Dimension(240, 20));
+        outlabel.setFont(new Font("Segoe UI", Font.BOLD, 20));
+        outlabel.setForeground(new Color(255, 0, 0));
+        outlabel.setPreferredSize(new Dimension(260, 20));
 
+        this.getContentPane().setBackground(new Color(255, 255, 192));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Count to 10");
         this.setLayout(layout);
@@ -45,16 +48,17 @@ public class CountTill10 extends JFrame implements ActionListener{
 
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == button){
-            if(count < 10){
+            if(count < 9){
                 count++;
                 tf.setText(""+count);
-                if(count >= 5){
+                if(count >= 3){
                     tf.setForeground(new Color(255, 255, 0));
                 }
-                if(count >= 10){
+                if(count >= 7){
                     tf.setForeground(new Color(0, 255, 0));
                 }
             } else{
+                tf.setText("10");
                 button.setEnabled(false);
                 outlabel.setText("Congratulations! You won.");
             }
