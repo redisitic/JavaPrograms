086e99b9b1e613a03324b954e335b91e1bb1b18f
Rhythm7845~ <66195447+redisitic@users.noreply.github.com>
2024-04-21 16:59:21 +0530

    :)

--- LCS.java
+++ LCS.java
@@ -1,39 +1,39 @@
 import java.util.*;
 public class LCS {
-    public static int longestCommonSubsequenceLength(String s1, String s2) {
+    public static int lcs(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
         Queue<Integer> Q = new LinkedList<>();
-        int matchingNumber = 0;
+        int matNum = 0;
         for(int i = 0; i <= n; i++){
-            Queue<Integer> currentRow = new LinkedList<>();
+            Queue<Integer> cRow = new LinkedList<>();
             for(int j = 0; j <= m; j++){
                 if(i == 0 || j == 0){
-                    currentRow.offer(0);
+                    cRow.offer(0);
                 } else{
                     if(s1.charAt(i - 1) == s2.charAt(j - 1)){
                         int diag = Q.poll();
-                        int previousValue = diag + 1;
-                        currentRow.offer(previousValue);
-                        if (previousValue > matchingNumber){
-                            matchingNumber = previousValue;
+                        int pVal = diag + 1;
+                        cRow.offer(pVal);
+                        if (pVal > matNum){
+                            matNum = pVal;
                         }
                     } else{
                         int diag = Q.poll();
-                        int verticalValue = currentRow.peek(); 
-                        int previousValue = Math.max(diag, verticalValue);
-                        currentRow.offer(previousValue);
+                        int vertVal = cRow.peek(); 
+                        int pVal = Math.max(diag, vertVal);
+                        cRow.offer(pVal);
                     }
                 }
             }
-            Q = currentRow;
+            Q = cRow;
         }
-        return matchingNumber;
+        return matNum;
     }
     public static void main(String[] args) {
         String s1 = "helloiamstupid";
         String s2 = "iamnothello";
-        int lcsLength = longestCommonSubsequenceLength(s1, s2);
+        int lcsLength = lcs(s1, s2);
         System.out.println(lcsLength); 
     }
 }
